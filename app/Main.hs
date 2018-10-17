8d5c325b9a8ced7a9d54dc0220300389fefde8b3
Leandro Galvan <leusgalvan@gmail.com>
2018-10-16 22:19:38 -0300

    Add major and minor version parsers

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Data.ByteString.Lazy as BL hiding (putStrLn)
 
 main :: IO ()
 main = do
-    let result = runGetOrFail parseMagicNumber (encode (0xCAFEBABE :: Word32))
+    let result = runGetOrFail parseMajorVersion (encode (44 :: Word16))
     case result of 
-      Left (_, _, s) -> putStrLn s
-      Right _ -> putStrLn "Success"
+      Left (_, _, s)  -> putStrLn s
+      Right (_, _, s) -> putStrLn (show s)
--- src/ClassFileParser.hs
+++ src/ClassFileParser.hs
@@ -1,12 +1,33 @@
-module ClassFileParser(parseMagicNumber) where
+module ClassFileParser(
+    parseMagicNumber,
+    parseMajorVersion,
+    parseMinorVersion
+) where
 
 import Data.Binary.Get
+import Data.Binary
 
-parseMagicNumber :: Get ()
+parseMagicNumber :: Get Word32
 parseMagicNumber = do
     let magicNumber = 0xCAFEBABE
     firstWord <- getWord32be
-    if firstWord == magicNumber then return ()
+    if firstWord == magicNumber then return firstWord
     else fail "Class file does not start with correct magic number"
+
+parseMajorVersion :: Get Word16
+parseMajorVersion = do
+    let (minVersion, maxVersion) = (45, 52)
+    version <- getWord16be
+    if version >= minVersion && version <= maxVersion then return version
+    else fail ("Major version must be between " ++ (show minVersion) ++
+               " and " ++ (show maxVersion))
+
+parseMinorVersion :: Get Word16
+parseMinorVersion = do
+    let (minVersion, maxVersion) = (0, 3)
+    version <- getWord16be
+    if version >= minVersion && version <= maxVersion then return version
+    else fail ("Minor version must be between " ++ (show minVersion) ++
+               " and " ++ (show maxVersion))
         
 
