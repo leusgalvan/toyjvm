ea23843ad745a8a88d18a15752668fd5d2429a09
Leandro Galvan <leusgalvan@gmail.com>
2018-10-24 22:27:38 -0300

    Add loading of classes via command line argument

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,25 @@ module Main(main) where
 import ClassFileParser
 import Data.Binary
 import Data.Binary.Get
-import Data.ByteString.Lazy as BL hiding (putStrLn)
+import qualified Data.ByteString.Lazy as BL (readFile)
 import GHC.Word
 import System.Directory
+import System.Environment
 
 main :: IO ()
 main = do
-    currentDir <- getCurrentDirectory
-    let testFilePath = currentDir ++ "\\app\\Test.class"
-    classFileContents <- BL.readFile(testFilePath)
+    args <- getArgs
+    let mainClass = head args
+    runProg mainClass
+
+classNameToPath :: String -> String
+classNameToPath className = "app\\" ++ className ++ ".class"
+
+runProg :: String -> IO ()
+runProg mainClass = do
+    let classFilepath = classNameToPath mainClass
+    classFileContents <- BL.readFile classFilepath
     let result = runGetOrFail parseClassFile classFileContents
-    case result of 
+    case result of
       Left l  -> putStrLn (show l)
       Right r -> putStrLn (show r)
