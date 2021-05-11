e5eae93ccb1a8efe9c2ea0ffed1843af22b7a726
Leandro Galvan <leusgalvan@gmail.com>
2021-05-10 22:36:46 -0300

    Implement IStore

--- src/Execution.hs
+++ src/Execution.hs
@@ -5,6 +5,9 @@ import Data.Either.Combinators
 import Text.Pretty.Simple (pPrint)
 import Data.ByteString.Lazy
 import Method
+import Control.Monad
+import Instruction
+import Frame
 
 execute :: ClassFile -> IO (Either String ())
 execute classFile =   
@@ -14,3 +17,11 @@ execute classFile =
 
 executeMethod :: ConstantPool -> MethodInfo -> IO (Either String ())
 executeMethod cp mi = print (fromMethodInfo cp mi) >> return (Right ())
+
+executeInstruction :: Instruction -> Frame -> IO Frame
+executeInstruction (IConst n) f = return (pushInt n f)
+executeInstruction (IStore n) f = return (storeInt n f)
+executeInstruction (GetStatic n) f = undefined
+executeInstruction (ILoad n) f = undefined 
+executeInstruction (InvokeVirtual n) f = undefined 
+executeInstruction Return f = undefined 
---
+++ src/Frame.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Frame where
+
+import Data.Binary
+import Data.ByteString.Lazy as BS
+import Control.Monad.Trans.State as S
+import Data.Binary.Get as G
+import qualified Data.Array as A
+
+data Frame = F {
+    opStack :: ByteString,
+    localVarArray :: A.Array Int Word32
+} deriving Show
+
+type FrameOp = State Frame
+
+pushInt :: Int -> Frame -> Frame
+pushInt n f@F{opStack} = f { opStack = encode n `append` opStack }
+
+popInt :: Frame -> (Frame, Int)
+popInt f@F{opStack} = (f', n)
+    where (opStack', n)  = stackPopInt opStack
+          f'             = f { opStack = opStack'}
+
+storeInt :: Int -> Frame -> Frame
+storeInt idx f@F{opStack, localVarArray} = f { opStack = opStack', localVarArray = localVarArray'}
+    where (opStack', n)  = stackPopInt opStack
+          localVarArray' = localVarArray A.// [(idx, fromIntegral n)]
+          
+stackPopInt :: ByteString -> (ByteString, Int)
+stackPopInt opStack = flip runGet opStack $ do
+    n <- getWord32be
+    rem <- getRemainingLazyByteString 
+    return (rem, fromIntegral n)
